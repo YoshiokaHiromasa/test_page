349f5130b8376d995c5519f5fa6500caf889628b
yoshioka <yoshioka@fez-inc.jp>
2019-03-05 18:16:08 +0900

    add sidebar

--- pages/index.vue
+++ pages/index.vue
@@ -48,16 +48,10 @@
                       </div>
 
                       <div class="table1">
-                        <div class="table">
-                          <table>
-                            <tr>
-                              <th>内容量</th><th>成分</th><th>返金補修</th><th>医薬部外品</th>
-                            </tr>
-                            <tr>
-                              <td><div class="STU">120ml<br>(1か月)</div></td><td>73種類</td><td><div calss="red">◎<br>（永久保証）</div></td><td>〇</td>
-                            </tr>
-                          </table>
-                        </div>
+                      </div>
+                      <div class="setsumei">
+                        最新成分のリデンシルや独自成分のペブプロミンαを配合しているので、今まで育毛が上手く
+                        行かなかった人におすすめ
                       </div>
 
                   </div>
@@ -70,7 +64,117 @@
         </div>
         <div class="col-3">
           <div class="side-bar">
-            ブロック２
+           <div class="ads">
+             <div class="ad1">
+               <img src="chapup.jpg" class="img-fluid">
+             </div>
+             <div class="ad2">
+               <img src="plantel.jpg" class="img-fluid">
+             </div>
+           </div>
+           <div class="sidebar-rank">
+             <div class="sidebar-rank-title WG">
+               商品一覧
+             </div>
+             <div class="sidebar-rank-contents">
+               <div class="sidebar-rank-content">
+                 <img src="chapup (1).jpg">
+                 <div class="sidebar-rank-sestumei">
+                   <a href="#" >チャップアップ</a>
+                   <br><div class="gold">★★★★★</div>
+                 </div>
+                 <div class="sidebar-rank-kaisetsu">
+                   業界No.1、今売れている人気の育毛剤。口コミでも圧倒的な利...
+                 </div>
+               </div>
+               <div class="sidebar-rank-content">
+                 <img src="plantel (1).jpg">
+                 <div class="sidebar-rank-sestumei">
+                   <a href="#" >プランテルEX</a>
+                   <br><div class="gold">★★★★★</div>
+                 </div>
+                 <div class="sidebar-rank-kaisetsu">
+                   最新成分のリデンシルや独自成分のペブプロミンαを配合してい...
+                 </div>
+               </div>
+               <div class="sidebar-rank-content">
+                 <img src="iqos.png">
+                 <div class="sidebar-rank-sestumei">
+                   <a href="#" >イクオス</a>
+                   <br><div class="gold">★★★★　</div>
+                 </div>
+                 <div class="sidebar-rank-kaisetsu">
+                   サプリが＋1,080円するだけで付いてくるのでセットがお得！体...
+                 </div>
+               </div>
+               <div class="sidebar-rank-content">
+                 <img src="finjia.jpg">
+                 <div class="sidebar-rank-sestumei">
+                   <a href="#" >フィンジア</a>
+                   <br><div class="gold">★★★★　</div>
+                 </div>
+                 <div class="sidebar-rank-kaisetsu">
+                   キャピキシル等一般的な育毛剤に配合されていない成分が高濃...
+                 </div>
+               </div>
+               <div class="sidebar-rank-content">
+                 <img src="polypure.jpg">
+                 <div class="sidebar-rank-sestumei">
+                   <a href="#" >ポリピュア</a>
+                   <br><div class="gold">★★★★　</div>
+                 </div>
+                 <div class="sidebar-rank-kaisetsu">
+                   男女兼用で幅広い世代に愛され、どんな抜け毛にも最適！人気...
+                 </div>
+               </div>
+             </div>
+             <div class="sidebar-ichiran-title WG">
+              ランキング一覧
+             </div>
+             <div class="sidebar-ichiran-contents">
+               <div class="sidebar-ichiran-content">
+                 <img src="emuji.png">
+                 <a href="#">Ｍ字型薄毛に効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="oji.png">
+                 <a href="#">O字型薄毛に効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="yuji.png">
+                 <a href="#">U字型薄毛に効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="kongo.png">
+                 <a href="#">混合型薄毛に効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="hosoge.png">
+                 <a href="#">細毛・軟毛に効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="zyosei.png">
+                 <a href="#">女性の薄毛に効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="wakahages.png">
+                 <a href="#">若ハゲに効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="emuji.png">
+                 <a href="#">抜け毛に効くランキング</a>
+               </div>
+               <div class="sidebar-ichiran-content">
+                 <img src="emuji.png">
+                 <a href="#">コスパが良いランキング</a>
+               </div>
+
+
+
+
+             </div>
+
+           </div>
           </div>
         </div>
       </div>
@@ -91,13 +195,62 @@ export default {
   }
 }
 
-
 </script>
 
 <style>
 
+.sidebar-ichiran-content img{
+  margin-left:1px;
+  height:21px;
+  width:21px;
+}
+
+.sidebar-ichiran-title{
+  margin-top:5px;
+padding-top:4px;
+padding-left:3px;
+}
+
+.sidebar-rank-kaisetsu{
+  font-size:15px;
+}
+
+.gold{
+  color:gold;
+}
+
+.sidebar-rank-sestumei{
+  float:right;
+  font-size:20px;
+}
+
+.sidebar-rank-content{
+  padding-top:10px;
+}
+.sidebar-rank-content img{
+  width:65px;
+  height:65px;
+}
+.sidebar-rank-title{
+  margin-top:5px;
+  padding-top:4px;
+  padding-left:3px;
+}
+
+.ads{
+  padding-top:10px;
+}
+
+.setsumei{
+  padding:10px 5px;
+}
+
 .table1{
   width:100%;
+  margin-top:15px;
+  text-align:center;
+  height:125px;
+  background-color:lightsteelblue;
 }
 
 .ranking-top{
@@ -161,11 +314,11 @@ export default {
 }
 
 .col-9{
-  background-color:snow;
+  background-color:gray;
 }
 
 .col-3{
-  background-color:gray;
+
 }
 
 </style>
