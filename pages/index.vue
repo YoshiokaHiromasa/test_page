79782176eedf4df5ec3534c15dcede2d9d987ddf
yoshioka <yoshioka@fez-inc.jp>
2019-03-06 13:42:32 +0900

    add ranking

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,12 @@
           みんなの育毛剤
         </div>
     </div>
+    <div class="">
 
+    </div>
     <div class="container">
       <div class="row">
-        <div class="col-9">
+        <div class="col-sm-9">
           <div class="main">
             <div class="main-header">
                <img src="top-header.png" class="img-fluid">
@@ -26,6 +28,8 @@
                   <img src="ranking-header.png" class="img-fluid">
                 </div>
                 <div class="ranking-contents">
+
+
                   <div class="ranking-content">
                     <div class="ranking-title">第１位　<a href="#">チャップアップ</a></div>
                       <div class="ranking-top">
@@ -46,23 +50,267 @@
 
                         </div>
                       </div>
-
-                      <div class="table1">
-                      </div>
+                          <div class="hyo">
+                          <table>
+                            <tr>
+                              <th>内容量</th><th>成分</th><th>返金補修</th><th>医薬部外品</th>
+                            </tr>
+                            <tr>
+                              <td><div class="STU">120ml<br>(1か月分)</div></td><td>73種類</td>
+                              <td><div calss="red">〇<br>（永久保証）</div></td><td>×</td>
+                            </tr>
+                          </table>
+                          </div>
+                      
                       <div class="setsumei">
-                        最新成分のリデンシルや独自成分のペブプロミンαを配合しているので、今まで育毛が上手く
-                        行かなかった人におすすめ
+                        年間販売実績が1位と圧倒的人気な育毛剤で初めて育毛剤を使う人や＋1,440円で付いてくるノンシリコンシャンプーを試したい人におすすめ
+                      </div>
+
+                      <div class="botans">
+                        <div class="row">
+                          <div class="col-6">
+                              <div class="btn btn1 btn-lg btn-block">
+                                チャップアップの<br>
+                                詳細ページへ
+                              </div>
+                          </div>
+                          <div class="col-6">
+                              <div class="btn  btn2 btn-lg btn-block" >
+                                  チャップアップの<br>
+                                  の公式ページへ
+                              </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                      <div class="mimi">
+                        <div class="mimi-title">
+                          ✔みん育.comだけの耳寄り情報！
+                        </div>
+                        <div class="mimi-content">
+                          Amazonや楽天だと古いものが出品されているリスクがあるため、
+                          買うなら公式ホームページが断然安心！しかも、公式ホームページからだと、全額返金・永久保証付きで購入可能！
+                        </div>
                       </div>
 
                   </div>
+
+                 <div class="ranking-content">
+                    <div class="ranking-title">第２位　<a href="#">プランテルEX</a></div>
+                      <div class="ranking-top">
+                        <div class="container">
+                          <div class="row">
+                            <div class="col-4">
+                              <img src="plantel (1).jpg" class="img-fluid">
+                            </div>
+                            <div class="col-8">
+                              <div class="ranking-naiyo">
+                                ★★★★★　4.5（口コミ320件）
+                                <br>販売価格：￥9,700
+                                <p class="red">割引率：今だけ33％ OFF</p>
+                              </div>
+                            </div>
+
+                          </div>
+
+                        </div>
+                      </div>
+                          <div class="hyo">
+                          <table>
+                            <tr>
+                              <th>内容量</th><th>成分</th><th>返金補修</th><th>医薬部外品</th>
+                            </tr>
+                            <tr>
+                              <td><div class="STU">50ml</div></td><td>42種類</td>
+                              <td><div calss="red">〇<br>（30日保証）</div></td><td>〇</td>
+                            </tr>
+                          </table>
+                          </div>
+                      
+                      <div class="setsumei">
+                        最新成分のリデンシルや独自成分のペブプロミンαを配合しているので、
+                        今までの育毛が上手くいかなかった人におすすめ
+                      </div>
+
+                      <div class="botans">
+                        <div class="row">
+                          <div class="col-6">
+                              <div class="btn btn1 btn-lg btn-block">
+                                プランテルEX<br>
+                                詳細ページへ
+                              </div>
+                          </div>
+                          <div class="col-6">
+                              <div class="btn  btn2 btn-lg btn-block" >
+                                  プランテルEx<br>
+                                  の公式ページへ
+                              </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                      <div class="mimi">
+                        <div class="mimi-title">
+                          ✔みん育.comだけの耳寄り情報！
+                        </div>
+                        <div class="mimi-content">
+                          安心安全の返金保障付き！送料無料で今だけ30%以上もお得なキャンペーン実施中！
+                        </div>
+                      </div>
+
+                  </div>
+                 <div class="ranking-content">
+                    <div class="ranking-title">第３位　<a href="#">イクオス</a></div>
+                      <div class="ranking-top">
+                        <div class="container">
+                          <div class="row">
+                            <div class="col-4">
+                              <img src="iqos.png" class="img-fluid">
+                            </div>
+                            <div class="col-8">
+                              <div class="ranking-naiyo">
+                                ★★★★★　4.3（口コミ200件）
+                                <br>販売価格：￥6,458
+                                <p class="red">割引率：今だけ49％ OFF</p>
+                              </div>
+                            </div>
+
+                          </div>
+
+                        </div>
+                      </div>
+                          <div class="hyo">
+                          <table>
+                            <tr>
+                              <th>内容量</th><th>成分</th><th>返金補修</th><th>医薬部外品</th>
+                            </tr>
+                            <tr>
+                              <td><div class="STU">120ml</div></td><td>61種類</td>
+                              <td><div calss="red">〇<br>（45日保証）</div></td><td>〇</td>
+                            </tr>
+                          </table>
+                          </div>
+                      
+                      <div class="setsumei">
+                        サプリが＋1,080円するだけで付いてくるので
+                        セットがお得！体内からも育毛したい人におすすめ
+                      </div>
+
+                      <div class="botans">
+                        <div class="row">
+                          <div class="col-6">
+                              <div class="btn btn1 btn-lg btn-block">
+                                イクオスの<br>
+                                詳細ページへ
+                              </div>
+                          </div>
+                          <div class="col-6">
+                              <div class="btn  btn2 btn-lg btn-block" >
+                                イクオスの<br>
+                                  の公式ページへ
+                              </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                      <div class="mimi">
+                        <div class="mimi-title">
+                          ✔みん育.comだけの耳寄り情報！
+                        </div>
+                        <div class="mimi-content">
+                          安心安全の返金保障付き！送料無料で約50%お得なキャンペーン実施中！
+                        </div>
+                      </div>
+
+                  </div>
+                 <div class="ranking-content">
+
+                    <div class="ranking-title">第４位　<a href="#">フィンジア</a></div>
+                      <div class="ranking-top">
+                        <div class="container">
+                          <div class="row">
+                            <div class="col-4">
+                              <img src="finjia.jpg" class="img-fluid">
+                            </div>
+                            <div class="col-8">
+                              <div class="ranking-naiyo">
+                                ★★★★★　4.2（口コミ96件）
+                                <br>販売価格：￥9,980
+                                <p class="red">割引率：今だけ22％ OFF</p>
+                              </div>
+                            </div>
+
+                          </div>
+
+                        </div>
+                      </div>
+                          <div class="hyo">
+                          <table>
+                            <tr>
+                              <th>内容量</th><th>成分</th><th>返金補修</th><th>医薬部外品</th>
+                            </tr>
+                            <tr>
+                              <td><div class="STU">50ml</div></td><td>13種類</td>
+                              <td><div calss="red">〇<br>（30日保証）</div></td><td>×</td>
+                            </tr>
+                          </table>
+                          </div>
+                      
+                      <div class="setsumei">
+                        キャピキシル等一般的な育毛剤に配合されていない成分
+                        が高濃度で配合されているので市販の育毛剤が効かなかった人におすすめ
+                      </div>
+
+                      <div class="botans">
+                        <div class="row">
+                          <div class="col-6">
+                              <div class="btn btn1 btn-lg btn-block">
+                                フィンジアの<br>
+                                詳細ページへ
+                              </div>
+                          </div>
+                          <div class="col-6">
+                              <div class="btn  btn2 btn-lg btn-block" >
+                                  フィンジアの<br>
+                                  の公式ページへ
+                              </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                      <div class="mimi">
+                        <div class="mimi-title">
+                          ✔みん育.comだけの耳寄り情報！
+                        </div>
+                        <div class="mimi-content">
+                          安心安全の返金保障付き！送料無料で今だけ20%以上もお得なキャンペーン実施中！
+                        </div>
+                      </div>
+
+                  </div>
+
+
+
+
+                </div>
+              </div>
+              <div class="hikaku">
+                <div class="hikaku-title WG">
+                  比較表
                 </div>
 
               </div>
+
+
             </div>
 
           </div>
         </div>
-        <div class="col-3">
+        <div class="col-sm-3">
           <div class="side-bar">
            <div class="ads">
              <div class="ad1">
@@ -161,15 +409,27 @@
                  <a href="#">若ハゲに効くランキング</a>
                </div>
                <div class="sidebar-ichiran-content">
-                 <img src="emuji.png">
+                 <img src="nukege.png">
                  <a href="#">抜け毛に効くランキング</a>
                </div>
                <div class="sidebar-ichiran-content">
                  <img src="emuji.png">
                  <a href="#">コスパが良いランキング</a>
                </div>
-
-
+            <div class="sidebar-koramu-title WG">
+              コラム一覧
+            </div>
+            <div class="sidebar-koramu-contents">
+               <div class="sidebar-koramu-content">
+                 <a href="#">なぜハゲるの？</a>
+               </div>
+                <div class="sidebar-koramu-content">
+                 <a href="#">育毛剤に頼りたくない？そんな人へ</a>
+               </div>
+                <div class="sidebar-koramu-content">
+                 <a href="#">育毛に効く食べもの10選</a>
+               </div>
+             </div>
 
 
              </div>
@@ -198,6 +458,98 @@ export default {
 </script>
 
 <style>
+
+.hikaku-title{
+  margin-top:10px;
+  padding-top:4px;
+  padding-left:5px;
+}
+
+.setsumei{
+  margin-top:5px;
+  margin-bottom:5px;
+  border: 1px solid lightgray;
+}
+
+.hyo{
+  margin-top:10px;
+}
+.hyo table {
+  width:100%;
+  border-collapse:  collapse;
+}
+
+.hyo th{
+  padding:13px 10px;
+  background-color:lightgray;
+  color:dimgray;
+  text-align: center;
+  border: solid 1px lightgray;
+}
+
+ .hyo td{
+  padding:10px 10px;
+  background-color:white;
+  margin:auto;
+  text-align: center;
+  border: solid 1px lightgray;
+}
+
+
+.ranking-content{
+  border-bottom:dashed 3px gray;
+  padding-bottom:15px;
+}
+
+.mimi-title{
+  background-color: whitesmoke;
+  text-align: center;
+  border: 1px solid whitesmoke;
+  height:35px;
+  padding-top:4px;
+}
+
+.mimi-content{
+ height:60px;
+ border: 1px solid whitesmoke;
+ padding-top:8px;
+ padding-left:5px;
+ padding-right:5px;
+}
+
+.mimi{
+  margin-top:10px;
+}
+
+.btn1:hover{
+  background-color:#38b48b;
+  color:white
+}
+
+.btn2:hover{
+  background-color:#e9546b;
+  color:white
+}
+
+.btn{
+  border-radius:5px;
+  height:110px;
+  padding-top:14px;
+  color:white;
+  font-size:28px;
+}
+
+.btn1{
+background-color: #00a497;
+}
+
+.btn2{
+background-color: #d3381c;
+}
+
+.sidebar-koramu-content{
+  padding-top:7px;
+}
 
 .sidebar-ichiran-content img{
   margin-left:1px;
@@ -311,10 +663,12 @@ padding-left:3px;
 
 .header-title{
   font-size:20px;
+  padding-top:3px;
+  padding-left:5px;
 }
 
 .col-9{
-  background-color:gray;
+  
 }
 
 .col-3{
