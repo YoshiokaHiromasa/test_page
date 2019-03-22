6656068ae23872d8b2b5c1348a8b7e5e1b1418e0
Hiromasa Yoshioka <hiromasa@Hiromasa-MacBook-Pro.local>
2019-03-22 17:10:49 +0900

    add footer and sidebar component

---
+++ components/footer.vue
@@ -0,0 +1,57 @@
+<template>
+   <div class="footer">
+      <div class="footer-content">
+        <div class="row">
+          <div class="col-3 d-none d-sm-block">
+            <div class="footer-items">
+              <div　class="footer-title"><a href="#">人気の育毛剤</a></div>
+              <div class="footer-list">
+                <a href="#">チャップアップ</a><br>
+                <a href="#">プランテルEX</a><br>
+                <a href="#">イクオス</a><br>
+                <a href="#">フィンジア</a><br>
+                <a href="#">ポリピュア</a><br>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 d-none d-sm-block">
+            <div class="footer-ranking">
+              <div　class="footer-title"><a href="#">人気のランキング</a></div>
+              <div class="footer-list">
+                <a href="#">M字型薄毛に効くランキング</a><br>
+                <a href="#">O字型薄毛に効くランキング</a><br>
+                <a href="#">U字型薄毛に効くランキング</a><br>
+                <a href="#">混合型薄毛に効くランキング</a><br>
+                <a href="#">細毛・軟毛に効くランキング</a><br>
+              </div>
+            </div>
+          </div>          
+          <div class="col-3 d-none d-sm-block">
+            <div class="footer-column">
+              <div　class="footer-title"><a href="#">コラム一覧</a></div>
+              <div class="footer-list">
+                <a href="#">なぜハゲるの？</a><br>
+                <a href="#">育毛剤に頼りたくない？そんな人へ</a><br>
+                <a href="#">育毛に効く食べ物10選</a><br>
+              </div>
+            </div>
+
+          </div>
+           <div class="col-3">
+             <div class="footer-about">
+               <div　class="footer-title"><a href="#">サイトに関して</a></div>
+              <div class="footer-list">
+                <a href="#">運営者情報</a><br>
+                <a href="#">調査概要</a><br>
+                <a href="#">プライバシーポリシー</a><br>
+              </div>
+             </div>
+          </div>
+        </div>
+      </div>
+    <div class="Copyright">
+        All rights reserved @ SunL
+    </div>
+
+</div>
+</template>
---
+++ components/sidebar.vue
@@ -0,0 +1,124 @@
+<template>
+       <div class="side-bar">
+        <div class="ads d-none d-sm-block">
+          <div class="ad1">
+            <img src="chapup.jpg" class="img-fluid">
+          </div>
+          <div class="ad2">
+            <img src="plantel.jpg" class="img-fluid">
+          </div>
+        </div>
+        <div class="sidebar-rank">
+          <div class="sidebar-rank-title ">
+            商品一覧
+          </div>
+          <div class="sidebar-rank-contents">
+            <div class="sidebar-rank-content">
+              <img src="chapup_1.jpg">
+              <div class="sidebar-rank-sestumei">
+                <a href="#" >チャップアップ</a>
+                <br><div class="gold">★★★★★</div>
+              </div>
+              <div class="sidebar-rank-kaisetsu">
+                業界No.1、今売れている人気の育毛剤。口コミでも圧倒的な利...
+              </div>
+            </div>
+            <div class="sidebar-rank-content">
+              <img src="plantel_1.jpg">
+              <div class="sidebar-rank-sestumei">
+                <a href="#" >プランテルEX</a>
+                <br><div class="gold">★★★★★</div>
+              </div>
+              <div class="sidebar-rank-kaisetsu">
+                最新成分のリデンシルや独自成分のペブプロミンαを配合してい...
+              </div>
+            </div>
+            <div class="sidebar-rank-content">
+              <img src="iqos.png">
+              <div class="sidebar-rank-sestumei">
+                <a href="#" >イクオス</a>
+                <br><div class="gold">★★★★　</div>
+              </div>
+              <div class="sidebar-rank-kaisetsu">
+                サプリが＋1,080円するだけで付いてくるのでセットがお得！体...
+              </div>
+            </div>
+            <div class="sidebar-rank-content">
+              <img src="finjia.jpg">
+              <div class="sidebar-rank-sestumei">
+                <a href="#" >フィンジア</a>
+                <br><div class="gold">★★★★　</div>
+              </div>
+              <div class="sidebar-rank-kaisetsu">
+                キャピキシル等一般的な育毛剤に配合されていない成分が高濃...
+              </div>
+            </div>
+            <div class="sidebar-rank-content">
+              <img src="polypure.jpg">
+              <div class="sidebar-rank-sestumei">
+                <a href="#" >ポリピュア</a>
+                <br><div class="gold">★★★★　</div>
+              </div>
+              <div class="sidebar-rank-kaisetsu">
+                男女兼用で幅広い世代に愛され、どんな抜け毛にも最適！人気...
+              </div>
+            </div>
+          </div>
+          <div class="sidebar-ichiran-title ">
+          ランキング一覧
+          </div>
+          <div class="sidebar-ichiran-contents">
+            <div class="sidebar-ichiran-content">
+              <img src="emuji.png">
+              <a href="#">Ｍ字型薄毛に効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="oji.png">
+              <a href="#">O字型薄毛に効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="yuji.png">
+              <a href="#">U字型薄毛に効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="kongo.png">
+              <a href="#">混合型薄毛に効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="hosoge.png">
+              <a href="#">細毛・軟毛に効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="zyosei.png">
+              <a href="#">女性の薄毛に効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="wakahages.png">
+              <a href="#">若ハゲに効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="nukege.png">
+              <a href="#">抜け毛に効くランキング</a>
+            </div>
+            <div class="sidebar-ichiran-content">
+              <img src="emuji.png">
+              <a href="#">コスパが良いランキング</a>
+            </div>
+        <div class="sidebar-koramu-title ">
+          コラム一覧
+        </div>
+          <div class="sidebar-koramu-contents">
+            <div class="sidebar-koramu-content">
+              <a href="#">なぜハゲるの？</a>
+            </div>
+            <div class="sidebar-koramu-content">
+              <a href="#">育毛剤に頼りたくない？そんな人へ</a>
+            </div>
+            <div class="sidebar-koramu-content">
+              <a href="#">育毛に効く食べもの10選</a>
+            </div>
+          </div>
+          </div>
+          </div>
+        </div>
+</template>
